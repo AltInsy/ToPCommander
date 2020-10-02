7de6794e57a3fdc2e5d88acc715f45130e519aba
AltInsy <70848671+AltInsy@users.noreply.github.com>
2020-10-02 22:32:44 +0200

    Add files via upload

---
+++ ToP Server Commander/ToPServerCommander.dpr
@@ -0,0 +1,29 @@
+// ---------------------------------------------------------------------------
+
+// This software is Copyright (c) 2015 Embarcadero Technologies, Inc.
+// You may only use this software if you are an authorized licensee
+// of an Embarcadero developer tools product.
+// This software is considered a Redistributable as defined under
+// the software license agreement that comes with the Embarcadero Products
+// and is subject to that software license agreement.
+
+// ---------------------------------------------------------------------------
+
+program ToPServerCommander;
+
+uses
+  Vcl.Forms,
+  Main_u in 'Source\Main_u.pas' {frmMain} ,
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Title := 'ToP Server Commander';
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+
+end.
